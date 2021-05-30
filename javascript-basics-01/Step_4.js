9605e61cfb0d28dc649b9df4d0dab082e34c8710
abbasskaouk <abbassikaouk@gmail.com>
2021-05-30 05:19:03 +0300

    Step_4 Strings Concatenation 2 Done

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,8 @@
+
+function X(){
+    var name = document.getElementById("name").value;
+    var surname = document.getElementById("surname").value;
+    var city = document.getElementById("city").value;
+
+    alert("Name : " + name + "\nSurname : " + surname + "\nCity : " + city);
+}
