f4454cb715f9ca65a5428a1730381f8044f3e0a2
abbasskaouk <abbassikaouk@gmail.com>
2021-05-30 05:23:19 +0300

    Step_5 Simple Math Done

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,8 @@
+function multiply(){
+    var fisrtnumber = document.getElementById("first_number").value;
+    var secondnumber = document.getElementById("second_number").value;
+
+    var answer = fisrtnumber * secondnumber ;
+
+    alert("Answer is : " + answer);
+}
