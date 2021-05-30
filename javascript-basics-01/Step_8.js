49a3023ffbbe9e37cdaa44bdd6d1d6d9718bcfd6
abbasskaouk <abbassikaouk@gmail.com>
2021-05-30 05:42:24 +0300

    Step_8 if, else Done

---
+++ javascript-basics-01/Step_8.js
@@ -0,0 +1,9 @@
+function ageAsk(){
+    var age = document.getElementById("age").value;
+
+    if( age > 18 ){
+        alert("You are over 18");
+    }
+    else
+        alert("You are under 18");
+}
