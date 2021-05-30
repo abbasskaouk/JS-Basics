12de95147e7dda65ad8182f9c657a1f05055ae7e
abbasskaouk <abbassikaouk@gmail.com>
2021-05-31 00:41:46 +0300

    Step_3 Done

---
+++ javascript-basics-03/step 3/Step_3.js
@@ -0,0 +1,13 @@
+var text = document.getElementById("text");
+  
+  function green(){
+    text.style.color = "green" ;
+  }
+
+  function red(){
+    text.style.color = "red" ;
+  }
+
+  function blue(){
+    text.style.color = "blue" ;
+  }
